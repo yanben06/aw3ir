2f51f5fa30bcb48efc0a4eedcddc6892b60edb8a
yanben06 <56534978+yanben06@users.noreply.github.com>
2019-11-18 11:21:19 +0100

    Update form-jquery-validation.js

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -1,13 +1,12 @@
 $( document ).ready(function() {
-    
+    $("form").on("submit", function (event) {
+        event.preventDefault();
+      
+​
+    });
     // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
     // voir plus : https://www.w3schools.com/js/js_htmldom.asp
      console.log( "DOM ready!" );
-     if (document.getElementById("nom").value.length<0 &&
-        document.getElementById("prenom").value.length <0 &&
-        document.getElementById("adresse").value.length <0 &&
-        document.getElementById("mail").value.length <0 )
-        document.getElementById("error").innerHTML = "La saisie du mail est obligatoire";
-
+     
     // Y mettre le code jQuery pour valider tous les champs du formulaire
  });
