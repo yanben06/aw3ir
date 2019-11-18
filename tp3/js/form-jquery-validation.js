36a3585acf24e644cc86385a9b50de22affdc2d5
yanben06 <56534978+yanben06@users.noreply.github.com>
2019-11-18 12:16:23 +0100

    Update form-jquery-validation.js

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -2,33 +2,21 @@ $(document).ready(function () {
     $("form").on("submit", function (event) {
         event.preventDefault();
         if ($("#nom").val() < 1) {
-            $(".modal-body").text("Chaine de caractère");
+            $(".modal-body").text("veuillez saisir votre nom");
             
             $('#myModal').modal("show");
         }
 
         if ($("#prenom").val() < 1) {
-            // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
-            // voir plus : https://www.w3schools.com/js/js_htmldom.asp
-            console.log("Veuillez remplir tous les champs");
-
-            // Y mettre le code jQuery pour valider tous les champs du formulaire
+            $(".modal-body").text("veuillez saisir votre prenom");
             $('#myModal').modal("show");
         }
         if ($("#adresse").val() < 1) {
-            // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
-            // voir plus : https://www.w3schools.com/js/js_htmldom.asp
-            console.log("Veuillez remplir tous les champs");
-
-            // Y mettre le code jQuery pour valider tous les champs du formulaire
+            $(".modal-body").text("veuillez saisir votre adresse");
             $('#myModal').modal("show");
         }
         if ($("#mail").val() < 1) {
-            // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
-            // voir plus : https://www.w3schools.com/js/js_htmldom.asp
-            console.log("Veuillez remplir tous les champs");
-
-            // Y mettre le code jQuery pour valider tous les champs du formulaire
+            $(".modal-body").text("veuillez saisir votre mail");
             $('#myModal').modal("show");
         }
 
