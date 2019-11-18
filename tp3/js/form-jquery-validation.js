4ce449d2e676e61a471b2d92ccbf92ecd9a9a7c3
yanben06 <56534978+yanben06@users.noreply.github.com>
2019-11-18 12:29:06 +0100

    Update form-jquery-validation.js

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -10,17 +10,17 @@ $(document).ready(function () {
         }
 
         if ($("#prenom").val() < 1) {
-            error += "veuillez saisir votre nom";
+            error += "veuillez saisir votre prenom";
             $(".modal-body").text("veuillez saisir votre prenom");
             $('#myModal').modal("show");
         }
         if ($("#adresse").val() < 1) {
-            error += "veuillez saisir votre nom";
+            error += "veuillez saisir votre adresse";
             // $(".modal-body").text("veuillez saisir votre adresse");
             $('#myModal').modal("show");
         }
         if ($("#mail").val() < 1) {
-            error += "veuillez saisir votre nom";
+            error += "veuillez saisir votre mail";
             //$(".modal-body").text("veuillez saisir votre mail");
         }
         if(error == ""){
