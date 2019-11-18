05b9491ff6d3a62ea9a5e0124961a720e475cf80
yanben06 <56534978+yanben06@users.noreply.github.com>
2019-11-18 12:26:08 +0100

    Update form-jquery-validation.js

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -1,25 +1,33 @@
 $(document).ready(function () {
     $("form").on("submit", function (event) {
+        var error = "";
         event.preventDefault();
         if ($("#nom").val() < 1) {
-            $(".modal-body").text("veuillez saisir votre nom");
+            error += "veuillez saisir votre nom";
+            // $(".modal-body").text("veuillez saisir votre nom");
             
-            $('#myModal').modal("show");
+            // $('#myModal').modal("show");
         }
 
         if ($("#prenom").val() < 1) {
+            error += "veuillez saisir votre nom";
             $(".modal-body").text("veuillez saisir votre prenom");
             $('#myModal').modal("show");
         }
         if ($("#adresse").val() < 1) {
-            $(".modal-body").text("veuillez saisir votre adresse");
+            error += "veuillez saisir votre nom";
+            // $(".modal-body").text("veuillez saisir votre adresse");
             $('#myModal').modal("show");
         }
         if ($("#mail").val() < 1) {
-            $(".modal-body").text("veuillez saisir votre mail");
+            error += "veuillez saisir votre nom";
+            //$(".modal-body").text("veuillez saisir votre mail");
             $('#myModal').modal("show");
         }
-else ($(".modal-body").text("Bienvenue Yanis") );
+        if(error == ""){
+            $(".modal-body").text("Bienvenue Yanis") ;
+        }
+
     });
 
 });
