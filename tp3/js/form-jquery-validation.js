323ecea153ee92d734bb57dbfeb8dba1e364d813
yanben06 <56534978+yanben06@users.noreply.github.com>
2019-11-18 11:58:32 +0100

    Update form-jquery-validation.js

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     $("form").on("submit", function (event) {
         event.preventDefault();
-        if ($("#nom").val() < 1) {
+       
+        if ($("#nom").val() < 1 & $("#prenom").val() < 1 & $("#adresse").val() < 1 & $("#mail").val() < 1) {
             // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
             // voir plus : https://www.w3schools.com/js/js_htmldom.asp
             console.log("Veuillez remplir tous les champs");
@@ -9,31 +10,7 @@ $(document).ready(function () {
             // Y mettre le code jQuery pour valider tous les champs du formulaire
             $('#myModal').modal("show");
         }
-
-        if ($("#prenom").val() < 1) {
-            // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
-            // voir plus : https://www.w3schools.com/js/js_htmldom.asp
-            console.log("Veuillez remplir tous les champs");
-
-            // Y mettre le code jQuery pour valider tous les champs du formulaire
-            $('#myModal').modal("show");
-        }
-        if ($("#adresse").val() < 1) {
-            // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
-            // voir plus : https://www.w3schools.com/js/js_htmldom.asp
-            console.log("Veuillez remplir tous les champs");
-
-            // Y mettre le code jQuery pour valider tous les champs du formulaire
-            $('#myModal').modal("show");
-        }
-        if ($("#mail").val() < 1) {
-            // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
-            // voir plus : https://www.w3schools.com/js/js_htmldom.asp
-            console.log("Veuillez remplir tous les champs");
-
-            // Y mettre le code jQuery pour valider tous les champs du formulaire
-            $('#myModal').modal("show");
-        }
+       
 
     });
 
