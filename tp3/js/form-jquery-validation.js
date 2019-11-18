8398a504df934b71c1288848464422744351e112
yanben06 <56534978+yanben06@users.noreply.github.com>
2019-11-18 11:31:20 +0100

    Update form-jquery-validation.js

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -1,12 +1,13 @@
 $( document ).ready(function() {
     $("form").on("submit", function (event) {
         event.preventDefault();
+      ("#name").val() < 1
       
 ​
     });
     // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
     // voir plus : https://www.w3schools.com/js/js_htmldom.asp
-     console.log( "DOM ready!" );
+     console.log( "Veuillez remplir tous les champs" );
      
     // Y mettre le code jQuery pour valider tous les champs du formulaire
  });
