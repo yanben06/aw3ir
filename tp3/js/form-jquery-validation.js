2692a2ffb12e41ba538b7af2afd34cd5996d9a1b
yanben06 <56534978+yanben06@users.noreply.github.com>
2019-11-18 11:03:04 +0100

    Update form-jquery-validation.js

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
-    // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
+    document.querySelector(".modal-body").innerHTML = '<img src="map.jpg"/>' // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
     // voir plus : https://www.w3schools.com/js/js_htmldom.asp
      console.log( "DOM ready!" );
      
-     // Y mettre le code jQuery pour valider tous les champs du formulaire
+     $(".modal-body").html('<img src="map.jpg"/>');// Y mettre le code jQuery pour valider tous les champs du formulaire
  });
