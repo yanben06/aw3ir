0e9787e80b163f066cac8fb2faa033288c2818d8
yanben06 <56534978+yanben06@users.noreply.github.com>
2019-11-18 10:49:16 +0100

    Create form-jquery-validation.js

---
+++ tp3/js/form-jquery-validation.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
+    // voir plus : https://www.w3schools.com/js/js_htmldom.asp
+     console.log( "DOM ready!" );
+     
+     // Y mettre le code jQuery pour valider tous les champs du formulaire
+ });
