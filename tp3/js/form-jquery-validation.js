d8845060e1e47fb3ade1561f8e122b87a908fa15
yanben06 <56534978+yanben06@users.noreply.github.com>
2019-11-24 14:06:15 +0100

    Update form-jquery-validation.js

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $("form").on("submit", function (event) {
         var error = "";
+        var a ="https://maps.googleapis.com/maps/api/staticmap?markers=Arcueil&zoom=10&size=400x300&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"/
         event.preventDefault();
         if ($("#nom").val() < 1) {
             error += "veuillez saisir votre nom";
@@ -26,7 +27,7 @@ $(document).ready(function () {
         if(error == ""){
                       //$(".modal-body").text("Bienvenue Yanis") ;
             $(".modal-title").text("Bievenue Yanis")
-            $(".modal-body").html("je suis né le "+ $("#date").val()+ " et j'habite à " +'<img src="https://maps.googleapis.com/maps/api/staticmap?markers=Arcueil&zoom=10&size=400x300&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"/ width="90%" >' + "" +'<a href="https://www.google.com/maps/place/19+Rue+Guy+de+Gouyon-du-Verger,+94110+Arcueil/@48.8036305,2.324284,17z/data=!3m1!4b1!4m5!3m4!1s0x47e6711a567a47d7:0x35307cc550fea4e0!8m2!3d48.803627!4d2.3264727" >Arcueil,Ile-De-France</a>' );
+            $(".modal-body").html("je suis né le "+ $("#date").val()+ " et j'habite à " +'<img src="https://maps.googleapis.com/maps/api/staticmap?markers=Arcueil&zoom=10&size=400x300&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg" width="90%" >' + "" +'<a href="https://www.google.com/maps/place/19+Rue+Guy+de+Gouyon-du-Verger,+94110+Arcueil/@48.8036305,2.324284,17z/data=!3m1!4b1!4m5!3m4!1s0x47e6711a567a47d7:0x35307cc550fea4e0!8m2!3d48.803627!4d2.3264727" >Arcueil,Ile-De-France</a>' );
            
         }else{
             $(".modal-body").text("veuillez saisir tous les champs") ;
