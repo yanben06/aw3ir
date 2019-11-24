d4df879ca0c473fe874ebd0162e5d23eeb769137
yanben06 <56534978+yanben06@users.noreply.github.com>
2019-11-24 14:28:49 +0100

    Update form-jquery-validation.js

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
         if(error == ""){
                       //$(".modal-body").text("Bienvenue Yanis") ;
             $(".modal-title").text("Bievenue Yanis")
-            $(".modal-body").html("je suis né le "+ $("#date").val()+ " et j'habite à " +'<img src= ' +a+ '  width="90%" >' + "" +'<a href='+ b+ '>Arcueil,Ile-De-France</a>' );
+            $(".modal-body").html("je suis né le "+ $("#date").val()+ " et j'habite à " +'<img src= ' +a+ '  width="90%" >' + "" +'<a href='+ b+ '>'+$("#adresse").val()+'</a>' );
            
         }else{
             $(".modal-body").text("veuillez saisir tous les champs") ;
