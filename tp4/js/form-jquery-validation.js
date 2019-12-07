46e6ccd073579ce15148f3acea4292bcc1a936ab
yanben06 <56534978+yanben06@users.noreply.github.com>
2019-12-07 01:14:26 +0100

    Update form-jquery-validation.js

--- tp4/js/form-jquery-validation.js
+++ tp4/js/form-jquery-validation.js
@@ -43,7 +43,7 @@ $(document).ready(function () {
              
     });
     $("#submit").click(function(){
-        contactStore.add(_nom, _prenom, _date, _adresse, _mail);
+        contactStore.add($("#nom"),$("#prenom"),$("#date"),$("#adresse"),$("#mail"));
         contactStore.getList();
         document.querySelector("table tbody").innerHTML = document.querySelector("table tbody").innerHTML +
         '<tr><td>'+$("#nom").val()+'</td><td>'+$("#prenom").val()+$("#date").val()+$("#adresse").val()+$("#mail").val()+'</td><td>';
