0a6af1e14e22a6a0cdda45c59e84b41f245b4fcc
yanben06 <56534978+yanben06@users.noreply.github.com>
2019-12-13 17:02:41 +0100

    Update formulaire.js

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -6,13 +6,13 @@ function Validation() {
     document.getElementById("resultat").innerHTML = ""; 
     document.getElementById("resultat").style.visibility = "hidden"; 
 
-    if (20<document.getElementById("txtNom").value.length < 5)
+    if (document.getElementById("txtNom").value.length < 5)
         document.getElementById("error").innerHTML = "La saisie du nom est obligatoire";
-    if (20<document.getElementById("txtPrenom").value.length < 5)
+    if (document.getElementById("txtPrenom").value.length < 5)
         document.getElementById("error").innerHTML = "La saisie du prénom est obligatoire";
-    if (50<document.getElementById("txtAdresse").value.length < 5)
-        document.getElementById("error").innerHTML = "La saisie du adresse est obligatoire";
-    if (30<document.getElementById("email").value.length < 5)
+    if (document.getElementById("txtAdresse").value.length < 5)
+        document.getElementById("error").innerHTML = "La saisie d'une adresse est obligatoire";
+    if (document.getElementById("email").value.length < 5)
         document.getElementById("error").innerHTML = "La saisie du mail est obligatoire";
 
     if (document.getElementById("txtNom").value.length >= 5 &&
